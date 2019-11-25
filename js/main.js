af86a09b965dab73cc834d072b4d99d87d01920f
Camatoz <yurabondik99@gmail.com>
2019-11-25 08:38:42 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ function test() {
     audio.innerHTML = "<source src=\"audio/а.mp3\" type=\"audio/mpeg\">";
     document.body.appendChild(audio);
 }
+
+// Прокрутка
+window.onload = function () {
+    // window.scrollTo(x,y)
+
+    document.getElementById('top').onclick = function () {
+        // console.log(window.pageYOffset);
+        window.scrollTo(0, 0);
+    }
+
+
+}
