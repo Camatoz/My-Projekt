119826907f8b6fe66a7c7c8ce9c9e7b20a1b5087
Camatoz <yurabondik99@gmail.com>
2019-11-25 14:05:07 +0300

    add functions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
-let letter = document.querySelector('a.letter');
+let alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
+let letterContainer = document.querySelector('div.letters');
 
-letter.addEventListener('click', function () {
-    letter.style.color = 'red';
-});
-
-function test() {
-    var audio = document.createElement('audio');
-    audio.setAttribute("autoplay", "true");
-    audio.innerHTML = "<source src=\"audio/а.mp3\" type=\"audio/mpeg\">";
-    document.body.appendChild(audio);
-}
 
 // Прокрутка
 window.onload = function () {
@@ -20,5 +11,44 @@ window.onload = function () {
         window.scrollTo(0, 0);
     }
 
+    renderLetters();
 
+}
+
+function renderLetters() {
+
+    for (let i = 0; i < alphabet.length; i++) {
+
+        // КОСТЫЛЬ
+        if (i == 30) {
+            let p = document.createElement('p');
+            letterContainer.appendChild(p);
+        }
+
+        // ДОБ ТЕГ А
+        let letter = document.createElement('a'); // созд вирт элемент А
+        letter.className = "letter"; // даем ему класс
+        letter.innerHTML = alphabet[i]; // вставляем в него ХТМЛ
+        letter.id = 'letter' + i; // даем ему ID
+        letterContainer.appendChild(letter); // добавляем его в страницу
+
+        // АЛЬТ ВАРИАНТ МЕДЛЕННЫЙ
+        // let letter = '<a href="#" class="letter" id="letter + ' + i + '">' + alphabet[i] + '</a>';
+        // letterContainer.innerHTML += letter;
+
+        // ДОБ ТЕГ АУДИО
+        let audio = document.createElement('audio');
+        audio.id = 'sound' + i;
+        audio.innerHTML = '<source src="audio/' + alphabet[i] + '.mp3" type="audio/mpeg">';
+        letterContainer.appendChild(audio);
+
+        // КЛИК ПО БУКВЕ
+        letter.addEventListener('click', function () {
+            let sound = document.querySelector('#sound' + i);
+            sound.play();
+            this.classList.add('visited');
+        });
+
+
+    }
 }
